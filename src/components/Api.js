15d21133a5ca5d19fbb5d6326ca650c16bd36ff7
Andrew <ajweaver18@gmail.com>
2024-06-23 11:55:32 -0400

    feat/ confirm modal and profile image button

---
+++ src/components/Api.js
@@ -0,0 +1,19 @@
+class Api {
+  constructor(options) {
+    // constructor body
+  }
+
+  getInitialCards() {
+    // ...
+  }
+
+  // other methods for working with the API
+}
+
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1",
+  headers: {
+    authorization: "40adfe4e-c0e4-40a1-9506-b555db893f72",
+    "Content-Type": "application/json",
+  },
+});
